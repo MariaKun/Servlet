0f5e189e32381f3c5d9b5bb5d8666931acb08b33
Maria Kun <penelopa.alex@yandex.ru>
2024-10-30 22:46:13 +0300

    add

---
+++ servlets/src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,46 @@
+package ru.netology.repository;
+
+import ru.netology.exception.NotFoundException;
+import ru.netology.model.Post;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+
+// Stub
+public class PostRepository {
+
+  private AtomicLong globalId = new AtomicLong(0);
+  private ConcurrentHashMap<Long, Post> hashMap = new ConcurrentHashMap<>();
+
+  public List<Post> all() {
+    return new ArrayList<>(hashMap.values());
+  }
+
+  public Optional<Post> getById(long id) {
+    return Optional.ofNullable(hashMap.get(id));
+  }
+
+  public Post save(Post post) {
+    if (post.getId() == 0)
+    {
+      var newId = globalId.getAndIncrement();
+      post.setId(newId);
+      hashMap.put(newId, post);
+    }
+    if (hashMap.containsKey(post.getId()))
+    {
+      hashMap.put(post.getId(), post);
+    }
+    else throw new NotFoundException("Id "+ post.getId() +" was not found");
+    return post;
+  }
+
+  public void removeById(long id) {
+    hashMap.remove(id);
+  }
+}
