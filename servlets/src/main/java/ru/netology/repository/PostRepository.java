cda810972ca2bf20c349930408f93c1921fd9109
Maria Kun <penelopa.alex@yandex.ru>
2024-10-31 00:37:28 +0300

    fix

--- servlets/src/main/java/ru/netology/repository/PostRepository.java
+++ servlets/src/main/java/ru/netology/repository/PostRepository.java
@@ -14,33 +14,31 @@ import java.util.concurrent.atomic.AtomicLong;
 // Stub
 public class PostRepository {
 
-  private AtomicLong globalId = new AtomicLong(0);
-  private ConcurrentHashMap<Long, Post> hashMap = new ConcurrentHashMap<>();
+    private AtomicLong globalId = new AtomicLong(0);
+    private ConcurrentHashMap<Long, Post> hashMap = new ConcurrentHashMap<>();
 
-  public List<Post> all() {
-    return new ArrayList<>(hashMap.values());
-  }
-
-  public Optional<Post> getById(long id) {
-    return Optional.ofNullable(hashMap.get(id));
-  }
-
-  public Post save(Post post) {
-    if (post.getId() == 0)
-    {
-      var newId = globalId.getAndIncrement();
-      post.setId(newId);
-      hashMap.put(newId, post);
+    public List<Post> all() {
+        return new ArrayList<>(hashMap.values());
     }
-    if (hashMap.containsKey(post.getId()))
-    {
-      hashMap.put(post.getId(), post);
-    }
-    else throw new NotFoundException("Id "+ post.getId() +" was not found");
-    return post;
-  }
 
-  public void removeById(long id) {
-    hashMap.remove(id);
-  }
+    public Optional<Post> getById(long id) {
+        return Optional.ofNullable(hashMap.get(id));
+    }
+
+    public Post save(Post post) {
+        if (post.getId() == 0) {
+            var newId = globalId.getAndIncrement();
+            post.setId(newId);
+            hashMap.put(newId, post);
+        }
+        if (hashMap.containsKey(post.getId())) {
+            hashMap.put(post.getId(), post);
+        } else throw new NotFoundException("Id " + post.getId() + " was not found");
+        return post;
+    }
+
+    public void removeById(long id) {
+      if (hashMap.remove(id) == null)
+        throw new NotFoundException("Id " + id + " was not found");;
+    }
 }
